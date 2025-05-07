e70764431bd853f1dbbbf122caa3344046979916
James Quoc Thai NGUYEN CAO <nguyencaojqt@gmail.com>
2025-05-07 16:12:43 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Obligatoire
-  trailingSlash: true, // Important pour S3
   images: {
-    unoptimized: true // Désactive l'optimisation d'image (nécessaire pour static)
+    domains: [],
   },
-  // Ajoutez ceci si vous utilisez des liens dynamiques
-  experimental: {
-    appDir: false // Désactive le nouveau système de routing si non utilisé
-  }
 };
 
 module.exports = nextConfig;
