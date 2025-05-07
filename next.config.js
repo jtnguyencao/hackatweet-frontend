b0783c41b3e8ba6201aef5242f2fe6a687a3631f
James Quoc Thai NGUYEN CAO <nguyencaojqt@gmail.com>
2025-05-07 11:50:43 +0200

    test1

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  output: 'export', // Required for static S3 hosting
+  trailingSlash: true, // Needed for proper S3 path resolution
 };
 
 module.exports = nextConfig;
