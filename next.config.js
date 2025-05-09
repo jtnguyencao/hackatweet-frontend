a8ac6a62c199235e1be6fddc3c5e6388560e5575
James Quoc Thai NGUYEN CAO <nguyencaojqt@gmail.com>
2025-05-09 12:22:39 +0200

    try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: "export", // Obligatoire
   trailingSlash: true, // Important pour S3
   images: {
